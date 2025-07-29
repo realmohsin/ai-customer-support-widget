c60705631febc6e79b711abcd6ef55ecdd5f7aa7
Real Mohsin <real@realmohsin.com>
2025-07-29 10:00:00 -0400

    initialize monorepo

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@workspace/ui/components/button";
+import { add } from "@workspace/math/add";
+import { Input } from "@workspace/ui/components/input";
+
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center min-h-svh">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-2xl font-bold">Hello World</h1>
+        <Button size="sm">Button</Button>
+        <p>{add(1, 2)}</p>
+        <Input />
+      </div>
+    </div>
+  );
+}
