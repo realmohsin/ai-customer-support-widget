eaeff54a7b323754d626ed5de380e0a4170177af
Real Mohsin <real@realmohsin.com>
2025-08-02 10:02:03 -0400

    add convex backend

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,15 +1,16 @@
-import { Button } from "@workspace/ui/components/button";
-import { add } from "@workspace/math/add";
-import { Input } from "@workspace/ui/components/input";
+"use client";
+
+import { useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
-        <p>{add(1, 2)}</p>
-        <Input />
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <p>apps/web</p>
+      <div className="max-w-sm w-full mx-auto">
+        <pre>{JSON.stringify(users, null, 2)}</pre>
       </div>
     </div>
   );
--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,12 +1,17 @@
-import { Button } from "@workspace/ui/components/button"
+"use client";
+
+import { useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <p>apps/widget</p>
+      <div className="max-w-sm w-full mx-auto">
+        <pre>{JSON.stringify(users, null, 2)}</pre>
       </div>
     </div>
-  )
-}
+  );
+
