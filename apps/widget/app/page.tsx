71df63a1bdef813f675a38079ca37af4d66b070b
Real Mohsin <real@realmohsin.com>
2025-08-03 11:00:02 -0400

    update for convex mutations

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,17 +1,20 @@
 "use client";
 
-import { useQuery } from "convex/react";
+import { useQuery, useMutation } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
+import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
   const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
       <p>apps/widget</p>
+      <Button onClick={() => addUser()}>Add User</Button>
       <div className="max-w-sm w-full mx-auto">
         <pre>{JSON.stringify(users, null, 2)}</pre>
       </div>
     </div>
   );
-
+}
