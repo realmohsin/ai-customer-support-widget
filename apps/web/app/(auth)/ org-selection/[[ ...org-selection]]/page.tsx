1d50e54db8061ad52054b933b04baafdd45f1de8
Real Mohsin <real@realmohsin.com>
2025-08-04 04:02:12 -0400

    organization requirement added for user

---
+++ apps/web/app/(auth)/ org-selection/[[ ...org-selection]]/page.tsx
@@ -0,0 +1,7 @@
+import { OrgSelectView } from "@/modules/auth/ui/views/org-selection-view";
+
+const Page = () => {
+  return <OrgSelectView />;
+};
+
+export default Page;
---
+++ apps/web/app/(dashboard)/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { useQuery, useMutation } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
+import { Button } from "@workspace/ui/components/button";
+
+export default function Page() {
+  const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add);
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <p>apps/web</p>
+        <Button onClick={() => addUser()}>Add User</Button>
+        <UserButton />
+        <OrganizationSwitcher hidePersonal={true} />
+        <div className="max-w-sm w-full mx-auto">
+          <pre>{JSON.stringify(users, null, 2)}</pre>
+        </div>
+      </div>
+    </>
+  );
+}
