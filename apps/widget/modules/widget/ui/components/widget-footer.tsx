51c1c37dced0fedfc4cde2cf1d3f36565cfa999d
Real Mohsin <real@realmohsin.com>
2025-08-06 04:10:13 -0400

    create session for widget user - database and ui

--- apps/widget/modules/widget/ui/components/widget-footer.tsx
+++ apps/widget/modules/widget/ui/components/widget-footer.tsx
@@ -28,7 +28,10 @@ export const WidgetFooter = () => {
         variant="ghost"
       >
         <InboxIcon
-          className={cn("size-5", screen === "inbox" && "text-primary")}
+          className={cn(
+            "size-5"
+            // screen === "inbox" && "text-primary"
+          )}
         />
       </Button>
     </footer>
