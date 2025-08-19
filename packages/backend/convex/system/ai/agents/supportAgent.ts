e4367ceca9a1d9e3f87c2399e2ffdc6cda5dbc42
Real Mohsin <real@realmohsin.com>
2025-08-19 05:03:01 -0400

    implement RAG for agent

--- packages/backend/convex/system/ai/agents/supportAgent.ts
+++ packages/backend/convex/system/ai/agents/supportAgent.ts
@@ -1,8 +1,8 @@
 import { openai } from "@ai-sdk/openai";
 import { Agent } from "@convex-dev/agent";
 import { components } from "../../../_generated/api";
-import { resolveConversation } from "../tools/resolveConversation";
-import { escalateConversation } from "../tools/escalateConversation";
+// import { resolveConversation } from "../tools/resolveConversation";
+// import { escalateConversation } from "../tools/escalateConversation";
 // import { SUPPORT_AGENT_PROMPT } from "../constants";
 
 export const supportAgent = new Agent(components.agent, {
@@ -10,8 +10,4 @@ export const supportAgent = new Agent(components.agent, {
   //   instructions: SUPPORT_AGENT_PROMPT,
   instructions:
     "You are a customer support agent. Use 'resolveConversation' tool when user expresses finalization of the conversation. Use 'escalateConversation' tool when user expresses frustration or requests a human explicitly.",
-  tools: {
-    resolveConversation,
-    escalateConversation,
-  },
 });
